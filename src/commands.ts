96165947e0be449997be47f5fcf8292b11965492
kabosnn <anagynandor@gmail.com>
2024-10-21 18:14:48 +0200

    add validation to list commands arguments

--- src/commands.ts
+++ src/commands.ts
@@ -2,11 +2,16 @@ import { Recipe, RecipeType } from "./recipe";
 import { Store } from "./stores/store.type";
 
 export async function list(store: Store<RecipeType[]>, args: string[]) {
+  if (args.length > 0) {
+    console.error("List command should not have any argument");
+    return;
+  }
+
   const recipe = new Recipe(store);
   const recipes = await recipe.readAll();
   const formatted = recipes
     .map((recipe) => `- [${recipe.id}] ${recipe.name}`)
-    .join('\n');
-  console.log('Your recipes:');
+    .join("\n");
+  console.log("Your recipes:");
   console.log(formatted);
 }
