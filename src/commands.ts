d495fc941b889a451c5c79ae476a412a8037df4a
kabosnn <anagynandor@gmail.com>
2024-10-21 18:24:30 +0200

    add details feature which logs a specific recepie's details based on id

--- src/commands.ts
+++ src/commands.ts
@@ -3,10 +3,9 @@ import { Store } from "./stores/store.type";
 
 export async function list(store: Store<RecipeType[]>, args: string[]) {
   if (args.length > 0) {
-    console.error("List command should not have any argument");
+    console.log("List command should not have any argument");
     return;
   }
-
   const recipe = new Recipe(store);
   const recipes = await recipe.readAll();
   const formatted = recipes
@@ -14,4 +13,19 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
     .join("\n");
   console.log("Your recipes:");
   console.log(formatted);
+}
+
+export async function details(store: Store<RecipeType[]>, args: string[]) {
+  const id = args[0]
+  if (args.length !== 1) {
+    console.log("You can only pass an ID as argument");
+    return;
+  }
+  const recipe = new Recipe(store);
+  const recipes = await recipe.readAll();
+  const selectedRecipe = recipes.find(
+    (recepie) => recepie.id === Number(id)
+  );
+
+  console.log(`ID: ${selectedRecipe.id} Name: ${selectedRecipe.name}`);
 }
